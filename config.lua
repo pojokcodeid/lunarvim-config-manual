450f17827830f4be99206933cf8ce0440e720735
asep komarudin <pojokcodeid@gmail.com>
2023-09-03 19:52:44 +0700

    update

--- config.lua
+++ config.lua
@@ -11,51 +11,51 @@ an executable
 local on_save = true
 local options = {
   backspace = vim.opt.backspace + { "nostop" }, -- Don't stop backspace at insert
-  clipboard = "unnamedplus", -- Connection to the system clipboard
-  cmdheight = 0, -- hide command line unless needed
-  completeopt = { "menuone", "noselect" }, -- Options for insert mode completion
-  copyindent = true, -- Copy the previous indentation on autoindenting
-  cursorline = true, -- Highlight the text line of the cursor
-  expandtab = true, -- Enable the use of space in tab
-  fileencoding = "utf-8", -- File content encoding for the buffer
-  fillchars = { eob = " " }, -- Disable `~` on nonexistent lines
-  history = 100, -- Number of commands to remember in a history table
-  ignorecase = true, -- Case insensitive searching
-  laststatus = 3, -- globalstatus
-  lazyredraw = true, -- lazily redraw screen
-  mouse = "a", -- Enable mouse support
-  number = true, -- Show numberline
-  preserveindent = true, -- Preserve indent structure as much as possible
-  pumheight = 10, -- Height of the pop up menu
-  relativenumber = true, -- Show relative numberline
-  scrolloff = 8, -- Number of lines to keep above and below the cursor
-  shiftwidth = 2, -- Number of space inserted for indentation
-  showmode = false, -- Disable showing modes in command line
-  showtabline = 2, -- always display tabline
-  sidescrolloff = 8, -- Number of columns to keep at the sides of the cursor
-  signcolumn = "yes", -- Always show the sign column
-  smartcase = true, -- Case sensitivie searching
-  splitbelow = true, -- Splitting a new window below the current one
-  splitright = true, -- Splitting a new window at the right of the current one
-  swapfile = false, -- Disable use of swapfile for the buffer
-  tabstop = 2, -- Number of space in a tab
-  termguicolors = true, -- Enable 24-bit RGB color in the TUI
-  timeoutlen = 300, -- Length of time to wait for a mapped sequence
-  undofile = true, -- Enable persistent undo
-  updatetime = 300, -- Length of time to wait before triggering the plugin
-  wrap = true, -- Disable wrapping of lines longer than the width of window
-  writebackup = false, -- Disable making a backup before overwriting a file
+  clipboard = "unnamedplus",                    -- Connection to the system clipboard
+  cmdheight = 0,                                -- hide command line unless needed
+  completeopt = { "menuone", "noselect" },      -- Options for insert mode completion
+  copyindent = true,                            -- Copy the previous indentation on autoindenting
+  cursorline = true,                            -- Highlight the text line of the cursor
+  expandtab = true,                             -- Enable the use of space in tab
+  fileencoding = "utf-8",                       -- File content encoding for the buffer
+  fillchars = { eob = " " },                    -- Disable `~` on nonexistent lines
+  history = 100,                                -- Number of commands to remember in a history table
+  ignorecase = true,                            -- Case insensitive searching
+  laststatus = 3,                               -- globalstatus
+  lazyredraw = true,                            -- lazily redraw screen
+  mouse = "a",                                  -- Enable mouse support
+  number = true,                                -- Show numberline
+  preserveindent = true,                        -- Preserve indent structure as much as possible
+  pumheight = 10,                               -- Height of the pop up menu
+  relativenumber = true,                        -- Show relative numberline
+  scrolloff = 8,                                -- Number of lines to keep above and below the cursor
+  shiftwidth = 2,                               -- Number of space inserted for indentation
+  showmode = false,                             -- Disable showing modes in command line
+  showtabline = 2,                              -- always display tabline
+  sidescrolloff = 8,                            -- Number of columns to keep at the sides of the cursor
+  signcolumn = "yes",                           -- Always show the sign column
+  smartcase = true,                             -- Case sensitivie searching
+  splitbelow = true,                            -- Splitting a new window below the current one
+  splitright = true,                            -- Splitting a new window at the right of the current one
+  swapfile = false,                             -- Disable use of swapfile for the buffer
+  tabstop = 2,                                  -- Number of space in a tab
+  termguicolors = true,                         -- Enable 24-bit RGB color in the TUI
+  timeoutlen = 300,                             -- Length of time to wait for a mapped sequence
+  undofile = true,                              -- Enable persistent undo
+  updatetime = 300,                             -- Length of time to wait before triggering the plugin
+  wrap = true,                                  -- Disable wrapping of lines longer than the width of window
+  writebackup = false,                          -- Disable making a backup before overwriting a file
   -- minimal number of screen columns either side of cursor if wrap is `false`
   -- guifont = "monospace:h17", -- the font used in graphical neovim applications
   guifont = "Source_Code_Pro:h17", -- the font used in graphical neovim applications
-  whichwrap = "bs<>[]hl", -- which "horizontal" keys are allowed to travel to prev/next line            -- which "horizontal" keys are allowed to travel to prev/next line
+  whichwrap = "bs<>[]hl",          -- which "horizontal" keys are allowed to travel to prev/next line            -- which "horizontal" keys are allowed to travel to prev/next line
 }
 for k, v in pairs(options) do
   vim.opt[k] = v
 end
-vim.opt.shortmess:append "c" -- don't give |ins-completion-menu| messages
-vim.opt.iskeyword:append "-" -- hyphenated words recognized by searches
-vim.opt.formatoptions:remove { "c", "r", "o" } -- don't insert the current comment leader automatically for auto-wrapping comments using 'textwidth', hitting <Enter> in insert mode, or hitting 'o' or 'O' in normal mode.
+vim.opt.shortmess:append "c"                         -- don't give |ins-completion-menu| messages
+vim.opt.iskeyword:append "-"                         -- hyphenated words recognized by searches
+vim.opt.formatoptions:remove { "c", "r", "o" }       -- don't insert the current comment leader automatically for auto-wrapping comments using 'textwidth', hitting <Enter> in insert mode, or hitting 'o' or 'O' in normal mode.
 vim.opt.runtimepath:remove "/usr/share/vim/vimfiles" -- separate vim plugins from neovim in case vim still in use
 vim.opt.title = true
 vim.opt.titlestring = "%<%F%=%l/%L - Pojok Code"
@@ -286,8 +286,8 @@ lvim.builtin.alpha.dashboard.section.footer.val = footer()
 
 lvim.plugins = {
   { "ellisonleao/gruvbox.nvim", event = "VeryLazy" },
-  { "manzeloth/live-server", event = "VeryLazy" },
-  { "mg979/vim-visual-multi", event = "VeryLazy" },
+  -- { "manzeloth/live-server", event = "VeryLazy" },
+  { "mg979/vim-visual-multi",   event = "VeryLazy" },
   {
     "CRAG666/code_runner.nvim",
     event = "VeryLazy",
@@ -343,7 +343,7 @@ lvim.plugins = {
   -- for check startuptime
   { "dstein64/vim-startuptime", cmd = "StartupTime" },
   -- for coloring pairs
-  { "p00f/nvim-ts-rainbow", event = "BufWinEnter", dependencies = "nvim-treesitter/nvim-treesitter" },
+  { "p00f/nvim-ts-rainbow",     event = "BufWinEnter", dependencies = "nvim-treesitter/nvim-treesitter" },
   {
     "echasnovski/mini.indentscope",
     version = false, -- wait till new 0.7.0 release to put it back on semver
@@ -530,6 +530,7 @@ lvim.lsp.installer.setup.ensure_installed = {
   "intelephense",
   "tailwindcss",
   -- "jdtls",
+  "stylua",
 }
 
 require("lvim.lsp.manager").setup "emmet_ls"
@@ -540,7 +541,7 @@ require("lvim.lsp.manager").setup "intelephense"
 
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
-  { command = "stylua", filetype = { "lua" } },
+  { command = "stylua",  filetype = { "lua" } },
   { command = "prettier" },
   { command = "black" },
   -- { command = "blade_formatter", filetype = { "php", "blade", "blade.php" } },
